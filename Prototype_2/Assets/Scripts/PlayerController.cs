3d9a7cabb8e89cd8db970c34cf8a7641d788a8c0
GlebShevchenko <shevchenkogleb032@gmail.com>
2025-07-29 00:49:48 +0300

    Prototype_2

---
+++ Prototype_2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    public GameObject projectivePrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ограничения поля для игрока
+        if (transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Запустить снаряд из игрока
+            Instantiate(projectivePrefab, transform.position, projectivePrefab.transform.rotation);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
