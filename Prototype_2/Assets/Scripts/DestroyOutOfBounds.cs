c7c9d384ec8614c9361bf2b2cbee2d62d68b62b3
GlebShevchenko <shevchenkogleb032@gmail.com>
2025-07-30 15:59:15 +0300

    1

--- Prototype_2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype_2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -20,6 +20,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         else if (transform.position.z < loverBound)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over!");
         }
 
 
